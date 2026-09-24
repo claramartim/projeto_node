c7186f830fa8f82aa7ef0b822139e15fdd433fb0
claramartim <271965@unifio.edu.br>
2026-09-24 01:49:59 +0000

    ajustes "src/produto.model.ts"

--- src/models/produto.model.ts
+++ src/models/produto.model.ts
@@ -1,13 +1,37 @@
-class Produto {
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+export interface IProduto{
     id: number;
     nome: string;
     preco: number;
-
-    constructor(id: number, nome: string, preco: number) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-    }
 }
 
+class Produto extends Model<IProduto> implements IProduto{
+        public id!: number;
+        public nome!: string;
+        public preco!: number;
+    }
+
+Produto.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+        nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+        preco: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+},
+  {
+    sequelize,
+    tableName: 'produtos',
+    timestamps: false,
+  }
+);
 export default Produto;
